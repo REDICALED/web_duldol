c5b8fc798fd202d3eda3a86e010a348fddc294ca
kiamkoysek <kiamkoysek@gmail.com>
2024-05-15 04:50:18 +0900

    vercel err

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useParams } from "react-router-dom";
 import PostComponent1 from "@/components/initpages/PostComponent1";
 import PostComponent2 from "@/components/initpages/PostComponent2";
