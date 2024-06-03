81dba367c603b36ead5b409f26f5da155c1d21da
kiamkoysek <kiamkoysek@gmail.com>
2024-06-03 15:03:41 +0900

    zoomslick left,right area

--- src/components/ZoomSlick.tsx
+++ src/components/ZoomSlick.tsx
@@ -39,17 +39,19 @@ export const ZoomSlick = (props: { images: string[], images_cap: string[] }) => 
       ))}
         <div className="w-full text-center justify-end flex text-white ">
           <div className="flex">
-          <div className="fixed bottom-1/2 mb-[-1.25vh] md:mb-[-2.5vh] left-0 ">
-              <img src={arrow_back} className={ currentImage > 0 ? "opacity-100 md:h-[5vh] h-[2.5vh]" : "opacity-0 pointer-events-none "} onClick={()=>setCurrentImage(currentImage-1)}></img>
+            
+          <div  onClick={()=>{ if(currentImage > 0 )setCurrentImage(currentImage-1)}} className=" fixed bottom-[20vh] md:w-[10vw] w-[15vw] left-0 h-[60vh]">
+            <img src={arrow_back} className={ currentImage > 0 ? " fixed bottom-1/2 mb-[-1.25vh] md:mb-[-1.5vh] left-0 opacity-100 md:h-[3vh] h-[2.5vh]" : "fixed bottom-1/2 mb-[-1.25vh] md:mb-[-2.5vh] left-0 opacity-0 pointer-events-none  "}></img>
             </div>
 
-            <div className="fixed bottom-1/2 mb-[-1.25vh] md:mb-[-2.5vh] right-0 ">
-              <img src={arrow_forward} className={ currentImage < images.length - 1 ? "opacity-100 md:h-[5vh] h-[2.5vh]" : "opacity-0 pointer-events-none  "} onClick={()=>setCurrentImage(currentImage+1)}></img>
+            <div onClick={()=>{ if(currentImage < images.length - 1)setCurrentImage(currentImage+1)}} className=" fixed bottom-[20vh] md:w-[10vw] w-[15vw] right-0 h-[60vh]">
+              <img src={arrow_forward} className={ currentImage < images.length - 1 ? " fixed bottom-1/2 mb-[-1.25vh] md:mb-[-1.5vh] right-0 opacity-100 md:h-[3vh] h-[2.5vh]" : "fixed bottom-1/2 mb-[-1.25vh] md:mb-[-2.5vh] right-0 opacity-0 pointer-events-none  "}></img>
             </div>
+
           </div>
         </div>
-        <div className="fixed top-[5vh] md:top-[0vh] mb-[-1.25vh] md:mb-[-2.5vh] right-0 ">
-              <img src={x_button} className={ "opacity-100 md:h-[5vh] h-[2.5vh]" } onClick={()=>setImageSlickZoom({SlickImages: [], index: 0})}></img>
+        <div onClick={()=>setImageSlickZoom({SlickImages: [], index: 0})} className=" fixed top-[5vh] md:top-[0vh] mb-[-2.5vh] md:mb-[-3vh] right-0 md:h-[6vh] h-[5vh] md:w-[5vw] w-[10vw] ">
+              <img src={x_button} className={ " fixed top-[5vh] md:top-[0vh] mb-[-1.25vh] md:mb-[-1.5vh] right-0 opacity-100 md:h-[3vh] h-[2.5vh]" } ></img>
         </div>
 
         </div>
