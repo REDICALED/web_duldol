3759a65063f17714e20ec198f4220cec594f5a14
redicaled <redicaled@gmail.com>
2024-09-17 11:55:50 +0900

    title resizer removed

--- src/components/TipTap/SetTitleImage.tsx
+++ src/components/TipTap/SetTitleImage.tsx
@@ -6,7 +6,6 @@ import { useRecoilState } from 'recoil';
 // import { getFunc } from '@/components/Git/GitFunc';
 import { useState } from 'react';
 import Datepicker from "tailwind-datepicker-react"
-import Resizer from "react-image-file-resizer";
 
  const SetTitleImage
   = (props:any) => {
@@ -14,20 +13,6 @@ import Resizer from "react-image-file-resizer";
     const [ titlefile , setTitleFile] = useState<File | null>(null);
     const [show, setShow] = useState < boolean > (false);
 	const [, setSelectedDate] = useState < Date | null > (null);
-    const resizeTitleFile = (file: File): Promise<File>  =>
-        new Promise((res) => {
-          Resizer.imageFileResizer(
-            file, // target file
-            1000, // maxWidth
-            1000, // maxHeight
-            "JPEG", // compressFormat : Can be either JPEG, PNG or WEBP.
-            60, // quality : 0 and 100. Used for the JPEG compression
-            0, // rotation
-            (uri) => res(uri as File), // responseUriFunc
-            "file" // outputType : Can be either base64, blob or file.(Default type is base64)	
-          );
-        }
-      );
     // const GetContents = async () => {
     //     const octokit = new Octokit({
     //       auth: import.meta.env.VITE_APP_TOKEN,
@@ -61,7 +46,7 @@ import Resizer from "react-image-file-resizer";
         <img className=" h-full" src={URL.createObjectURL(titlefile)} alt="title"/>
             <input onChange={async (event)=>{
             if (event.target.files !== null){
-                const file = await resizeTitleFile(event.target.files[0]);
+                const file = await event.target.files[0];
 
                 // Blob URL 가져오기
                 const fileBlob = URL.createObjectURL(file);
@@ -82,7 +67,7 @@ import Resizer from "react-image-file-resizer";
             className="flex h-full w-full cursor-pointer flex-col items-center justify-center">
             <span>타이틀 사진 업로드</span>
             <input onChange={async (event)=>{if (event.target.files !== null){
-                const file = await resizeTitleFile(event.target.files[0]);
+                const file = await event.target.files[0];
 
                 // Blob URL 가져오기
                 const fileBlob = URL.createObjectURL(file);
